b9b9ca5230af686fc838a6210aa47bc2f651768e
Hack Reactor Student <student@hackreactor.com>
2019-07-15 22:36:07 -0700

    completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+//create an array to store the result in
+//return a function that checks if classList contains the classname, if it does push into the array
+//loop through each childnode to check if element type matches className using recursion
+//add child node to array if child node class matches className
+//return array
+
+var getElementsByClassName = function(className, node = document.body) {
   // your code here
+  var arrayByClass = [];
+  if (node.classList && node.classList.contains(className)) {
+    arrayByClass.push(node);
+  }
+  if (node.children) {
+    for (var i = 0; i < node.children.length; i++) {
+      arrayByClass = arrayByClass.concat(getElementsByClassName(className, node.children[i]));
+    }
+  }
+  return arrayByClass;
 };
